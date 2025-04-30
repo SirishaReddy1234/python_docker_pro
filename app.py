4b166300f7e6722810c75d2415ab7ac4fe9ffb32
SirishaReddy1234 <bandravusirisha@gmail.com>
2025-04-30 16:51:16 +0530

    this are dockerfile and python file

---
+++ app.py
@@ -0,0 +1,14 @@
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return """
+    <html>
+        <body style="background-color: lightblue;">
+            <h1 style="text-align: center; color: white;">Welcome to my web page!</h1>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
